1f7a76e0461c501bc84715f489c613e3fef0f649
vip-delete <vip.delete@gmail.com>
2024-12-29 19:52:20 +0100

    fix

--- scripts/server.js
+++ scripts/server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.use(serveStatic("public", { index: ["index.html"] }));
 
-app.listen(3001);
+app.listen(3000);
 console.log("Listening \x1b[92mhttp://localhost:3000\x1b[0m");
